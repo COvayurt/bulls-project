1850725d38774e677e9cf0a2a45044bac5e52951
COvayurt <canberk.ovayurt@gmail.com>
2017-11-23 14:59:15 +0300

    port, host arrangement

--- src/main/java/undertow/RequestServer.java
+++ src/main/java/undertow/RequestServer.java
@@ -2,26 +2,20 @@ package undertow;
 
 import io.undertow.Handlers;
 import io.undertow.Undertow;
-import io.undertow.server.HttpServerExchange;
-import io.undertow.util.Headers;
 import undertow.handler.FindTickersHandler;
 import undertow.handler.GenerateTickerFromBullsHandler;
 
 public class RequestServer {
 
-    public static void helloWorldHandler(HttpServerExchange exchange) {
-        exchange.getResponseHeaders().add(Headers.CONTENT_TYPE, "application/json");
-        exchange.getResponseSender().send("Hello World!");
-    }
 
     public static void main(String[] args) {
 
-        int port = 8080;
-        String host = "localhost";
+        String host = "0.0.0.0";
+        String port = System.getenv("PORT");
 
         Undertow server = Undertow
                 .builder()
-                .addHttpListener(port,
+                .addHttpListener(Integer.parseInt(port),
                         host,
                         Handlers.path()
                                 .addExactPath("/tickers/generate", new GenerateTickerFromBullsHandler())
